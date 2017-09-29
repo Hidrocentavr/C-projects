907464e900fa83a4502c83b17d891b3e71026109
Hidrocentavr <inbox@maksimshevchenko.ru>
2017-09-30 01:54:09 +0300

    Add new class "manipulator" and list of points

--- KinematicTask/main.cpp
+++ KinematicTask/main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-const float q_value[] = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+/*const float q_value[] = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
 //Таймер
 class MyTime
@@ -36,14 +36,55 @@ public:
             return q;
         }
     };
+};*/
+
+//манипулятор
+class Manipulator
+{
+private:
+    struct point;
+
+public:
+    int NumberOfPoints;
+    point points[NumberOfPoints];
+
+    void input()
+    {
+        cout << "Количество звеньев: ";
+        cin >> pointsnumber >> endl;
+        for (int i = 0; i < NumberOfPoints; i++)
+        {
+            cout << "Звено " << i << endl;
+            cout << "Тип кинематической пары (0 - поступательная, 1 - вращательная): ";
+            cin >> points[i].type >> endl;
+            if (points[i].type == 0)
+            {
+                cout << "Введите параметры Денавита-Хартенберга:" << endl;
+                cout << "Параметр q = ";
+                cin >> points[i].q >> endl;
+                cout << "Параметр alpha = ";
+                cin >> points[i].alpha >> endl;
+                cout << "Параметр a = ";
+                cin >> points[i].a >> endl;
+            }
+            else
+            {
+                cout << "Введите параметры Денавита-Хартенберга (если параметр" << endl;
+                cout << "Параметр alpha = ";
+                cin >> points[i].alpha >> endl;
+                cout << "Параметр a = ";
+                cin >> points[i].a >> endl;
+                cout << "Параметр d = ";
+                cin >> points[i].d >> endl;
+            }
+        }
+    }
 };
 
-//Решатель
+/*//Решатель
 class RobotArmSolver
 {
 private:
-    int number_of_points;
-    int point[][]; //звено(тип звена, система координат звена)
 
 public:
     //Вычисление матрицы перехода по известным обобщённым координатам
@@ -55,6 +96,77 @@ public:
         T = q; //вычисление матрицы
         return T;
     }
+};*/
+
+/*ЗВЕНО*/
+struct Point
+{
+    int type;
+    float q;
+    float alpha;
+    float a;
+    float d;
+
+    void Input(Point &point);
+
+    Point *Next;
+};
+
+/*РАБОТА СО СПИСКАМИ ЗВЕНЬЕВ*/
+class List
+{
+    Point *Head;
+
+public:
+    List()
+    {
+        Head = NULL;
+    }
+
+    ~List()
+    {
+        while (Head != NULL)
+        {
+            Point *temp = Head->Next;
+            delete Head;
+            Head = temp;
+        }
+    }
+
+    void Add(Point &point)
+    {
+        Point *temp = new Point;
+        temp->Next = Head;
+
+        temp->type = point.type;
+        temp->a = point.a;
+        temp->alpha = point.alpha;
+        temp->d = point.d;
+        temp->q = point.q;
+
+        Head = temp;
+    }
+
+    void Show()
+    {
+        Point *temp = Head;
+
+        while (temp != NULL)
+        {
+            cout << temp->type << endl;
+            cout << temp->a << endl;
+            cout << temp->alpha << endl;
+            cout << temp->d << endl;
+            cout << temp->q << endl;
+
+            temp = temp->Next;
+        }
+    }
+
+    void Remove()
+    {
+        Po
+    }
 };
 
 int main(int argc, char *argv[])
@@ -63,6 +175,8 @@ int main(int argc, char *argv[])
     setlocale(0, "");
 
     cout << "Hello world!" << endl;
+    manipulator::input();
+    cout << "The end!" << endl;
 
     return a.exec();
 }
