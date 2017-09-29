f71b59c2e8e4a18fe07a8e165e5f8a155306faa3
Hidrocentavr <inbox@maksimshevchenko.ru>
2017-09-30 00:20:36 +0300

    add some changes

--- KinematicTask/main.cpp
+++ KinematicTask/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 const float q_value[] = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
+//Таймер
 class MyTime
 {
 private:
@@ -14,11 +15,13 @@ public:
 
 };
 
+//Датчики
 class Sensors
 {
 private:
 
 public:
+    //энкодеры
     class encoders
     {
     private:
@@ -35,16 +38,20 @@ public:
     };
 };
 
+//Решатель
 class RobotArmSolver
 {
 private:
     int number_of_points;
-    int point[][];
+    int point[][]; //звено(тип звена, система координат звена)
 
 public:
-    //вычисление матрицы перехода по известным обобщённым координатам
+    //Вычисление матрицы перехода по известным обобщённым координатам
     float SolveDirectKinematic(float* q)
     {
+
+
+
         T = q; //вычисление матрицы
         return T;
     }
@@ -56,9 +63,6 @@ int main(int argc, char *argv[])
     setlocale(0, "");
 
     cout << "Hello world!" << endl;
-
-
-
 
     return a.exec();
 }
