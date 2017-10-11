e98b131bbe6d38c6372915361a5152789a5ccf60
Hidrocentavr <inbox@maksimshevchenko.ru>
2017-10-11 12:43:15 +0300

    Абстрактные классы. Виртуальные и чисто виртуальные методы. Пример.

--- lesson4/main.cpp
+++ lesson4/main.cpp
@@ -1,3 +1,4 @@
+/*
 //в C для реализации модуля необходимо создавать 2 различные функции
 int abs(int x);
 float fabs(float x);
@@ -5,12 +6,93 @@ float fabs(float x);
 //в C++ для реализации модуля можно перегружать одну функцию
 float abs(int x);
 float abs(float x);
+*/
 
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <string>
+#include <fstream>
 
-using namespace std;
+//интерфейс логгера
+class logger //абстрактный класс, так как имеется виртуальный метод
+{
+
+public:
+    virtual void log(const std::string &message = 0); //виртуальный метод
+
+    void log(const std::exception &ex)
+    {
+        log(std::string(ex.what()));
+    }
+
+    virtual ~logger() {}
+};
+
+//реализация консольного логгера
+class consolelogger : public logger
+{
+
+public:
+    void log(const std::string &message) override
+    {
+        std::cout << message << std::endl;
+    }
+};
+
+//реализация файлового логгера
+class filelogger : public logger
+{
+
+public:
+    filelogger(const std::string &filename)
+    {
+        _file.open(fileName);
+    }
+
+    void log(const std::string &message) override
+    {
+        _file << message << std.endl;
+    }
+
+    ~filelogger()
+    {
+        _file.close();
+    }
+
+private:
+    std::ofstream _file;
+};
+
+class arrlogger : public logger
+{
+public:
+    arrlogger(const std::string &config)
+    {
+        int count = 2;
+        arr = new logger * [2];
+        arr[0] = new consolelogger();
+        arr[1] = new filelogger("SomeName");
+    }
+
+    void log(const std::string &message) override
+    {
+        for (int i =0; i < count; i++)
+        {
+            arr[i]->log(message);
+        }
+    }
+
+    ~arrlogger()
+    {
+        delete (consolelogger) arr[0];
+        delete (filelogger) arr[1];
+        delete arr;
+    }
+
+private:
+    logger **arr;
+};
 
 //базовый класс
 class Base
@@ -25,6 +107,10 @@ class Base
     {
         cout << "Base method 2" << endl;
     }
+
+    virtual void method3() = 0; //чисто виртуальный метод
+
+    virtual void method4() {} //виртуальный метод
 };
 
 //класс наследник 1
