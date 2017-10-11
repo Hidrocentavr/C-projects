28e0449e03da42c0f50c74c5c7cf6bab924e72ba
Hidrocentavr <inbox@maksimshevchenko.ru>
2017-10-11 11:50:08 +0300

    Полиморфизм для явных общих интерфейсов

--- lesson4/main.cpp
+++ lesson4/main.cpp
@@ -6,10 +6,48 @@ float fabs(float x);
 float abs(int x);
 float abs(float x);
 
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
+//базовый класс
+class Base
+{
+    void method1();
+    void method2();
+};
+
+//класс наследник 1
+class Der1 : public Base
+{
+
+};
+
+//класс наследник 2
+class Der2 : public Base
+{
+
+};
+
+void polymorth(Base *x)
+{
+    x->method1();
+}
+
+void test()
+{
+    Der1 der1;
+    Der2 der2;
+
+    polymorth(&der1);
+    polymorth(&der2);
+}
 
 int main()
 {
+
+
+
     return 0;
 }
 
